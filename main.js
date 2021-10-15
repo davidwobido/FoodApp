2725c349916208a46c05b5e3beef54e328a9a95f
davidwobido <davidwobido@googlemail.com>
2021-10-15 12:24:54 +0200

    Add Selection Component

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 import { createTitleComponent } from "./lib/titleComponent.js";
-// import { createSelectionComponent } from "./lib/";
+import { createSelectionComponent } from "./lib/selectionComponent.js";
 // import { createFooterComponent } from "./lib/";
 
 function createApp() {
   const appElement = document.querySelector("#app");
 
   const titleComponent = createTitleComponent();
-  //   const selectionComponent = createSelectionComponent();
+  const selectionComponent = createSelectionComponent();
   //   const footerComponent = createFooterComponent();
 
   appElement.append(titleComponent);
-  //   appElement.append(selectionComponent);
+  appElement.append(selectionComponent);
   //   appElement.append(footerComponent);
 }
 createApp();
